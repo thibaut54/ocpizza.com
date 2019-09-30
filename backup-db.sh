8bc0a128a48526595b2b54aa8c9f1be2593d27e9
JMilton <john_milton100@hotmail.com>
2019-09-30 02:05:53 +0200

    rename files

--- backup-db.sh
+++ backup-db.sh
@@ -6,6 +6,5 @@ USER="admin_ocp"
 PGPASSWORD="admin"
 DATE=`date +%Y%m%d`
 FILENAME="/opt/db_backup/${DB}_${DATE}.sql"
-# -E UTF-8
+
 PGPASSWORD=${PGPASSWORD} pg_dump -Fc -U ${USER} -h ${HOST} ${DB} > ${FILENAME}
-#gzip ${FILENAME}
