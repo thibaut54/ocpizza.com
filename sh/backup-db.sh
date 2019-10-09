e6bed18341cddf750a8330b1d88c83bb38088474
JMilton <john_milton100@hotmail.com>
2019-10-09 20:19:10 +0200

    add documentation

--- sh/backup-db.sh
+++ sh/backup-db.sh
@@ -5,6 +5,8 @@ DB="db_ocpizza"
 USER="admin_ocp"
 PGPASSWORD="admin"
 DATE=`date +%Y%m%d`
-FILENAME="/opt/db_backup/${DB}_${DATE}.sql"
+FILENAME="/opt/db_backup/${DB}_${DATE}.tar"
+#FILENAME="/opt/db_backup/${DB}_${DATE}.sql"
 
-PGPASSWORD=${PGPASSWORD} pg_dump -Fc -U ${USER} -h ${HOST} ${DB} > ${FILENAME}
+PGPASSWORD=${PGPASSWORD} pg_restore -d ${DB} ${FILENAME} -c -U ${USER}
+#PGPASSWORD=${PGPASSWORD} pg_dump -Fc -U ${USER} -h ${HOST} ${DB} > ${FILENAME}
--- sh/extract-backup.sh
+++ sh/extract-backup.sh
@@ -2,8 +2,8 @@
 
 DB="db_wtc"
 DATE=`date +%Y%m%d`
-CONT_PATH="/opt/db_backup/${DB}_${DATE}.sql"
+CONT_PATH="/opt/db_backup/${DB}_${DATE}.tar"
 CONT_NAME="cont-db-wtc"
-DESTINATION="/opt/wtc/${DB}_${DATE}.sql"
+DESTINATION="/opt/wtc/${DB}_${DATE}.tar"
 # -E UTF-8
 docker cp ${CONT_NAME}:${CONT_PATH} ${DESTINATION}
